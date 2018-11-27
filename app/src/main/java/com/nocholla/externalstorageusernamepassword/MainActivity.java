34fd1a97b668b6088fcb47c743052083a889a9d8
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2018-11-26 21:28:59 -0800

    Added Get External Files

--- app/src/main/java/com/nocholla/externalstorageusernamepassword/MainActivity.java
+++ app/src/main/java/com/nocholla/externalstorageusernamepassword/MainActivity.java
@@ -1,5 +1,6 @@
 package com.nocholla.externalstorageusernamepassword;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -8,12 +9,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
     // Widgets
     private EditText inputUsername;
     private EditText inputPassword;
     private Button btnLogin;
+    FileOutputStream fstream;
+    Intent intent;
 
     /**
      * @method onCreate
@@ -37,6 +46,30 @@ public class MainActivity extends AppCompatActivity {
 
                 final String password = inputPassword.getText().toString();
                 Log.d("DEBUG PASSWORD", password);
+
+                intent = new Intent(MainActivity.this, UserDetailsActivity.class);
+                startActivity(intent);
+
+                // Save to External File
+                try {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},23);
+                    File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                    File myFile = new File(folder,"credentials");
+                    fstream = new FileOutputStream(myFile);
+                    fstream.write(username.getBytes());
+                    fstream.write(password.getBytes());
+                    fstream.close();
+
+                    Toast.makeText(getApplicationContext(), "Details Saved in " + myFile.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+
+                    //intent = new Intent(MainActivity.this, UserDetailsActivity.class);
+                    //startActivity(intent);
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
                 // Username Empty Validation
                 if (TextUtils.isEmpty(username)) {
